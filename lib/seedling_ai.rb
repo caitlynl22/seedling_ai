26063f8eb17288ad0e7fb1a9e56ed2ac459ce477
Caitlyn Landry <caitlyn.landry@gmail.com>
2025-10-29 18:54:26 -0400

    Initial commit

---
+++ lib/seedling_ai.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "seedling_ai/version"
+
+module SeedlingAi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/seedling_ai_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe SeedlingAi do
+  it "has a version number" do
+    expect(SeedlingAi::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
