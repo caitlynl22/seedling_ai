74ad35478c8a51d85f4ffa26da09faff4dc93cba
Caitlyn Landry <caitlyn.landry@gmail.com>
2025-11-06 02:12:17 -0500

    Configure logging

--- lib/seedling_ai.rb
+++ lib/seedling_ai.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require "logger"
 require_relative "seedling_ai/version"
 require_relative "seedling_ai/utils"
 
@@ -7,5 +8,19 @@ require "seedling_ai/railtie" if defined?(Rails)
 
 module SeedlingAi
   class Error < StandardError; end
-  # Your code goes here...
+  class << self
+    attr_writer :logger
+
+    def logger
+      @logger ||= if defined?(Rails) && Rails.respond_to?(:logger) && Rails.logger
+                    Rails.logger
+                  else
+                    Logger.new($stdout)
+                  end
+    end
+
+    def configure
+      yield self
+    end
+  end
 end
